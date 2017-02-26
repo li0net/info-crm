46add0d8be3bc4f674c58d4f5cc4899f5c29884f
ruslan <ruslan.cyb@gmail.com>
2017-02-26 16:49:24 +0300

    Доработка виджета

--- resources/views/widget/pages/times.blade.php
+++ resources/views/widget/pages/times.blade.php
@@ -2,9 +2,9 @@
 <h4>Онлайн-запись</h4>
 
 @for ($i = 0; $i < count($times); $i++)
-    <a class="row time-row" data-id="{{ $i }}" data-name="{{$times[$i]->work_start}} - {{$times[$i]->work_end}}"  href="#">
+    <a class="row time-row" data-id="{{$times[$i]}}" data-name="{{$times[$i]}}"  href="#">
         <div class="col-sm-11">
-            {{$times[$i]->work_start}} - {{$times[$i]->work_end}}
+            {{$times[$i]}}
         </div>
         <div class="col-sm-1"> <i class="fa fa-chevron-right" aria-hidden="true"></i> </div>
     </a>
